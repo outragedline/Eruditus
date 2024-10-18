bd05c7c00bc90eb52b165d43db5d25f842a881d6
outragedline <outragedline@proton.me>
2024-10-17 22:59:34 -0300

    feat(infra): add user and name parameters to ConnectionManager()

--- app/src/main/java/com/eruditus/infra/ConnectionManager.java
+++ app/src/main/java/com/eruditus/infra/ConnectionManager.java
@@ -6,13 +6,17 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 	private String url;
+	private String user;
+	private String password;
 
-	public ConnectionManager(String url) {
+	public ConnectionManager(String url, String user, String password) {
 		this.url = url;
+		this.user = user;
+		this.password = password;
 	}
 
 	public Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(url);
+		return DriverManager.getConnection(url, user, password);
 	}
 
 	public void closeConnection(Connection conn) {
