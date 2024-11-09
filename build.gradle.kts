88be3d8106ecbc1d55d8b10605a0e389897c71af
outragedline <outragedline@proton.me>
2024-11-09 01:16:43 -0300

    fix(build): add missing dependency for validations

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.5")
 }
 
 tasks.withType<Test> {
