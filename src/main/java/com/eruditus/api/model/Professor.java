686072865fcdd2bdd7192b81cb7582cb68fc2222
taylo <taylo@DESKTOP-CFPT2PI>
2024-11-09 22:17:40 -0300

    Implementing Professor Model

---
+++ src/main/java/com/eruditus/api/model/Professor.java
@@ -0,0 +1,85 @@
+package com.eruditus.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Entity
+public class Professor {
+    
+    @Id
+    @GeneratedValue
+    @Column (unique = true)
+    private long id;
+    
+    @NotEmpty
+    private String nome;
+    
+    @NotEmpty
+    private long idade;
+    
+    @NotEmpty
+    private String formacao;
+    
+    @NotEmpty
+    private String sexo;
+    
+    @NotEmpty
+    private String funcao;
+    
+    @OneToMany
+    private List<Contatos>contatos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getIdade() {
+        return idade;
+    }
+
+    public void setIdade(long idade) {
+        this.idade = idade;
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public List<Contatos> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<Contatos> contatos) {
+        this.contatos = contatos;
+    }
+
+}
