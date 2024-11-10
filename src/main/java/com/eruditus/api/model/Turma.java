505bb4806e609d8a6fc3b3cf599fd206c0bcc67f
taylo <taylo@DESKTOP-CFPT2PI>
2024-11-09 22:19:16 -0300

    Implementing Turma Model

---
+++ src/main/java/com/eruditus/api/model/Turma.java
@@ -0,0 +1,42 @@
+package com.eruditus.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Entity
+public class Turma {
+    
+    @Id
+    @GeneratedValue
+    @Column (unique = true)
+    private long id;
+    
+    @NotEmpty
+    private long vagas;
+    
+    @ManyToOne
+    private List<Aluno> aluno;
+
+    public long getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(long vagas) {
+        this.vagas = vagas;
+    }
+
+    public List<Aluno> getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(List<Aluno> aluno) {
+        this.aluno = aluno;
+    }
+    
+    
+}
