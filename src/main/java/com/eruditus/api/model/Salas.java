f5aa72d5c65c8da23dd79226023c1a82033b1761
taylo <taylo@DESKTOP-CFPT2PI>
2024-11-09 22:20:11 -0300

    Implementing Salas Model

---
+++ src/main/java/com/eruditus/api/model/Salas.java
@@ -0,0 +1,51 @@
+package com.eruditus.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+public class Salas {
+    
+    @Id
+    @GeneratedValue
+    @Column (unique = true)
+    private long id;
+    
+    @NotEmpty
+    private long capacide;
+    
+    @NotEmpty
+    private String drescricao;
+    
+    @NotEmpty
+    private String recursos;
+
+    public long getCapacide() {
+        return capacide;
+    }
+
+    public void setCapacide(long capacide) {
+        this.capacide = capacide;
+    }
+
+    public String getDrescricao() {
+        return drescricao;
+    }
+
+    public void setDrescricao(String drescricao) {
+        this.drescricao = drescricao;
+    }
+
+    public String getRecursos() {
+        return recursos;
+    }
+
+    public void setRecursos(String recursos) {
+        this.recursos = recursos;
+    }
+    
+    
+}
