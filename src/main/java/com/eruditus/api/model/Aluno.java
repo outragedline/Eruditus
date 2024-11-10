e6729cc719f3599b7a1cb273fb6ba6e0152bf62b
taylo <taylo@DESKTOP-CFPT2PI>
2024-11-09 22:14:53 -0300

    List and cardinality implementation in Aluno model

--- src/main/java/com/eruditus/api/model/Aluno.java
+++ src/main/java/com/eruditus/api/model/Aluno.java
@@ -1,11 +1,17 @@
 package com.eruditus.api.model;
 
 import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotEmpty;
 
+@Entity
 public class Aluno implements Serializable {
     
     private static final long serrialVersionUID = 1L;
@@ -21,8 +27,14 @@ public class Aluno implements Serializable {
     
     @NotEmpty
     private Integer idade;
-
-
+    
+    @ManyToOne
+    private Turma turma;
+    
+    @OneToMany(mappedBy = "aluno", cascade = CascadeType.REMOVE)
+    
+    private List<Contatos> contatos;
+    
     public String getNome() {
         return nome;
     }
@@ -46,6 +58,23 @@ public class Aluno implements Serializable {
     public void setIdade(Integer idade) {
         this.idade = idade;
     }
+
+    public List<Contatos> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<Contatos> contatos) {
+        this.contatos = contatos;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+    
     
     
 }
