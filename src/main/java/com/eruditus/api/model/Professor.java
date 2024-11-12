b437309664e81480d6dd564a0d69aea5fcf24899
taylo <taylo@DESKTOP-CFPT2PI>
2024-11-12 10:50:26 -0300

    Implementing Serializable in Professor model

--- src/main/java/com/eruditus/api/model/Professor.java
+++ src/main/java/com/eruditus/api/model/Professor.java
@@ -1,5 +1,6 @@
 package com.eruditus.api.model;
 
+import java.io.Serializable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +10,7 @@ import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Entity
-public class Professor {
+public class Professor implements Serializable {
     
     @Id
     @GeneratedValue
