d71dd479e1a34baea0d568b72f1d5f8c5b0615d9
aluno.senai <aluno.senai@C308-LAB3-021.senaiba.com.br>
2024-11-13 16:11:58 -0300

    IMPLEMENTING IMPORT'S IN ALUNO REPOSITORY

--- src/main/java/com/eruditus/api/repository/AlunoRepository.java
+++ src/main/java/com/eruditus/api/repository/AlunoRepository.java
@@ -1,13 +1,19 @@
 package com.eruditus.api.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.eruditus.api.model.Aluno;
+import com.eruditus.api.model.Administrador;
+import com.eruditus.api.model.Contatos;
+import com.eruditus.api.model.Cursos;
+import com.eruditus.api.model.Turma;
 
 public interface AlunoRepository extends CrudRepository<Aluno, String>{
     
-    Aluno findByCodigo(long id);
-    List<Aluno> findByNome(String nome);
+   
+    
+    
     
 }
