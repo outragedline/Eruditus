5ed509b12f3b27350506b39fe041bd79bd84d8d0
outragedline <outragedline@proton.me>
2024-11-22 02:41:35 -0300

    feat: create AuthService

---
+++ src/main/java/com/eruditus/api/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.eruditus.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.eruditus.api.dto.auth.SignUpDTO;
+import com.eruditus.api.model.User;
+import com.eruditus.api.model.UserRole;
+import com.eruditus.api.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class AuthService {
+	@Autowired
+	private UserRepository repository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Transactional
+	public User signUp(SignUpDTO data) {
+		if (repository.findByEmail(data.email()).isPresent()
+				|| repository.findByUsername(data.username()).isPresent()) {
+			throw new RuntimeException("Tried to create a user that already exists!");
+		}
+
+		User user = new User();
+		user.setEmail(data.email());
+		user.setUsername(data.username());
+		user.setPassword(passwordEncoder.encode(data.password()));
+		user.setRole(UserRole.USER);
+		return repository.save(user);
+	}
+}
