69a172274114ab788523d60ab7c0e650e84f6a71
outragedline <outragedline@proton.me>
2024-12-06 07:53:09 -0300

    feat: new endpoint to verify if token is valid

--- src/main/java/com/eruditus/api/controller/AuthController.java
+++ src/main/java/com/eruditus/api/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,5 +58,10 @@ public class AuthController {
 		}
 
 		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	@GetMapping("verifyToken")
+	public ResponseEntity<?> verifyToken() {
+		return ResponseEntity.ok("Token valid");
 	}
 }
